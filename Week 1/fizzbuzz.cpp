de8b725e9f65fd32f8b2b1d3c626275b7a809c31
MikeMwai <mwai.michael@strathmore.edu>
2023-08-15 15:48:39 +0300

    Update

--- Week 1/fizzbuzz.cpp
+++ Week 1/fizzbuzz.cpp
@@ -1,8 +1,19 @@
+/* Given an integer n, return a string array answer (1-indexed) where:
+ answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ answer[i] == "Fizz" if i is divisible by 3.
+ answer[i] == "Buzz" if i is divisible by 5.
+ answer[i] == i (as a string) if none of the above conditions are true.
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
 class Solution {
 public:
-    vector<string> fizzBuzz(int n) {
-        vector<string> answer;
-        
+    std::vector<std::string> fizzBuzz(int n) {
+        std::vector<std::string> answer;
+
         for (int i = 1; i <= n; ++i) {
             if (i % 3 == 0 && i % 5 == 0) {
                 answer.push_back("FizzBuzz");
@@ -11,10 +22,25 @@ public:
             } else if (i % 5 == 0) {
                 answer.push_back("Buzz");
             } else {
-                answer.push_back(to_string(i));
+                answer.push_back(std::to_string(i));
             }
         }
-        
+
         return answer;
     }
 };
+
+int main() {
+    Solution solution;
+    int n;
+    
+    std::cout << "Enter a value for n: ";
+    std::cin >> n;
+
+    std::vector<std::string> result = solution.fizzBuzz(n);
+    for (const std::string& s : result) {
+        std::cout << s << " ";
+    }
+
+    return 0;
+}
--- Week 1/sortingsentence.cpp
+++ Week 1/sortingsentence.cpp
@@ -1,3 +1,10 @@
+/*A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+*/
+
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -26,3 +33,12 @@ public:
         return sortedSentence;
     }
 };
+
+int main() {
+    Solution solution;
+    std::string originalSentence = "This4 is3 a2 sentence1";
+    std::string shuffledSentence = solution.sortSentence(originalSentence);
+    std::cout << "Original Sentence: " << originalSentence << std::endl;
+    std::cout << "Shuffled Sentence: " << shuffledSentence << std::endl;
+    return 0;
+}
